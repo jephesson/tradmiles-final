a3683104d91d525be99727cd63a3dbee5d3fdb05
Jephesson Alex Floriano dos Santos <jephessonalex@192.168.68.104>
2025-12-15 13:25:42 -0300

    fix: prisma binary engine and prisma client singleton

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,14 @@
-// src/lib/prisma.ts
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error", "warn"], // opcional: "query"
+    log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
